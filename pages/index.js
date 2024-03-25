8909eca8ce0df8e8f01197b1790ab1e64e9a5959
Daniel Vogel <54858106+vogelcodes@users.noreply.github.com>
2024-03-25 17:31:07 +0000

    remove-lakto-add-innerHtml

--- pages/index.js
+++ pages/index.js
@@ -6,38 +6,47 @@ const links = [
   {
     name: "Agende um consultoria comigo",
     url: "https://wa.me/5521986388688?text=Oi%2C+tudo+bem%3F+Eu+gostaria+de+agendar+uma+consultoria.",
+    img: "/links/1.webp",
   },
   {
-    name: "LactoFlow - Aumente a sua produção de leite materno",
+    name: "Aula Gratuita 👇<br/>LactoFlow - Aumente a sua produção de leite materno",
     url: "https://pv.lactoflow.com.br/?utm_source=bio",
+    img: "/links/2.webp",
   },
   {
     name: "Workshop - A Amamentação começa na Gestação",
     url: "https://go.hotmart.com/Y89675102O?dp=1&utm_source=bio",
+    img: "/links/3.webp",
   },
   {
     name: "E-book Guia de Retorno ao Trabalho SEM Desmame",
     url: "https://go.hotmart.com/B89175649F?dp=1&utm_source=bio",
+    img: "/links/4.webp",
   },
   {
     name: "Workshop - Desmame Gentil: Toda Amamentação tem seu Fim",
     url: "https://hotmart.com/pt-br/marketplace/produtos/workshop-toda-amamentacao-tem-seu-fim/C89898767H?utm_source=bio",
+    img: "/links/5.webp",
   },
-  {
-    name: "Cupom de Desconto para utilizar na Lackto: procaci ",
-    url: "https://www.lackto.com.br/?ref=procaci",
-  },
+  // {
+  //   name: "Cupom de Desconto para utilizar na Lackto: procaci ",
+  //   url: "https://www.lackto.com.br/?ref=procaci",
+  //   img: "/links/6.webp",
+  // },
   {
     name: "Página da Amazon de Carolina Procaci ",
     url: "https://www.amazon.com.br/shop/carolina.procaci?utm_source=bio",
+    img: "/links/7.webp",
   },
   {
     name: "Meu canal no Youtube",
     url: "https://www.youtube.com/channel/UCqQ4PyvfWuEIbtiU_aNh0rQ?sub_confirmation=1&utm_source=bio",
+    img: "/links/8.webp",
   },
   {
     name: "Encontre o Banco de Leite Humano mais próximo de você",
     url: "https://rblh.fiocruz.br/localizacao-dos-blhs?utm_source=bio",
+    img: "/links/9.webp",
   },
 ];
 
@@ -66,28 +75,32 @@ export default function Home() {
         </div>
 
         <div className="flex  text-white flex-col w-full space-y-4 mx-auto my-4 justify-between">
-          {links.map((link, i) => (
-            <a
-              href={`${link.url}${
-                searchParams != "" ? `&${searchParams}` : ""
-              }`}
-              target="_blank"
-            >
-              <div
-                key={link.name}
-                className="bg-blue rounded-md px-2 py-10 border-2 border-green text-cream duration-200 h-16 place-items-center justify-between flex"
+          {links.map((link, i) => {
+            const markup = { __html: link.name };
+            return (
+              <a
+                key={i}
+                href={`${link.url}${
+                  searchParams != "" ? `&${searchParams}` : ""
+                }`}
+                target="_blank"
               >
-                <Image
-                  src={`/links/${i + 1}.webp`}
-                  width={48}
-                  height={48}
-                  alt={`link-${i}`}
-                ></Image>
-                <p className="font-semibold w-2/3">{link.name}</p>
-                <div></div>
-              </div>
-            </a>
-          ))}
+                <div className="bg-blue rounded-md px-2 py-10 border-2 border-green text-cream duration-200 h-16 place-items-center justify-between flex">
+                  <Image
+                    src={link.img}
+                    width={48}
+                    height={48}
+                    alt={`link-${i}`}
+                  ></Image>
+                  <p
+                    className="font-semibold w-2/3"
+                    dangerouslySetInnerHTML={markup}
+                  ></p>
+                  <div></div>
+                </div>
+              </a>
+            );
+          })}
         </div>
       </main>
 
